0379e45e695dbb783ea15bc36a5dd2eb1621e044
soybison <coeneedell@gmail.com>
2021-07-01 15:50:08 -0500

    minimal app

--- scripts/build_database.py
+++ scripts/build_database.py
@@ -1,1 +1,11 @@
 import sqlalchemy
+import pandas as pd
+import os
+
+DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = DATABASE_URL.replace('postgres://', 'postgresql://', 1)
+database = sqlalchemy.create_engine(DATABASE_URL, connect_args={'sslmode': 'require'})
+
+if __name__ == '__main__':
+    df = pd.read_csv('../all_data_paintingsforCoen.csv')
+    df.to_sql('artworks', database)
