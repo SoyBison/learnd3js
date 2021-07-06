53e7659e99420b4f944a9cfce75b3602fa7196b1
soybison <coeneedell@gmail.com>
2021-07-06 14:15:22 -0500

    minimal commit, construction page and hidden test page

--- artic_fox/__init__.py
+++ artic_fox/__init__.py
@@ -1,13 +1,18 @@
-from flask import Flask, current_app
+from flask import Flask, render_template
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 
 
+@app.route("/testing_page")
+def testing_page():
+    return render_template('testing.html')
+
+
 @app.route("/")
-def hello_world():
-    return current_app.send_static_file('home.html')
+def home():
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run() 
